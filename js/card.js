2e5d405d4562b467eedaa015f8d05447e9984ea5
Pavel Melnik <SESA117590@schneider-electric.com>
2017-04-15 21:41:44 +0300

    Завершено разбиение на модули

--- js/card.js
+++ js/card.js
@@ -2,51 +2,57 @@
 // модуль отрисовки элемента на карточке
 'use strict'
 
-// Шаблон для панели с информацией по жилью
-var lodgeTemplate = document.querySelector('#lodge-template').content;
+window.card = (function (window, document) {
+  // Шаблон для панели с информацией по жилью
+  var lodgeTemplate = document.querySelector('#lodge-template').content;
 
-/**
- * Передать русское название типа жилья
- */
-function translateOfferType (offerType) {
+  /**
+   * Передать русское название типа жилья
+   */
+  function translateOfferType(offerType) {
     switch (offerType) {
-    case window.HOUSE_TYPES[0]: return 'Квартира';
-    case window.HOUSE_TYPES[1]: return 'Бунгало';
-    case window.HOUSE_TYPES[2]: return 'Дом';
-    default: return 'Unknown type';
+      case 'flat': return 'Квартира';
+      case 'bungalo': return 'Бунгало';
+      case 'house': return 'Дом';
+      default: return 'Unknown type';
+    }
   }
-}
 
-/**
- * Перевод массива опций жилья из [string] в фрагмент из HTML [<span>]
- * @param {*} string features массив опций жилья
- */
-function features2HTML (features) {
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < features.length; i++) {
-    var featureElement = document.createElement('span');
-    featureElement.className = 'feature__image feature__image--' + features[i];
-    fragment.appendChild(featureElement);
+  /**
+   * Перевод массива опций жилья из [string] в фрагмент из HTML [<span>]
+   * @param {*} string features массив опций жилья
+   */
+  function features2HTML(features) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < features.length; i++) {
+      var featureElement = document.createElement('span');
+      featureElement.className = 'feature__image feature__image--' + features[i];
+      fragment.appendChild(featureElement);
+    }
+    return fragment;
   }
-  return fragment;
-}
 
-/**
- * Генератор HTML из шаблона на основе JS объекта предложения
- * @param {*} deal Объект JS с полным описанием предложения
- */
-function renderOffer (deal) {
-  var offerElement = lodgeTemplate.cloneNode(true);
+  /**
+   * Генератор HTML из шаблона на основе JS объекта предложения
+   * @param {*} deal Объект JS с полным описанием предложения
+   */
+  function renderOffer(deal) {
+    var offerElement = lodgeTemplate.cloneNode(true);
 
-  offerElement.querySelector('.lodge__title').textContent = deal.offer.title;
-  offerElement.querySelector('.lodge__address').textContent = deal.offer.address;
-  offerElement.querySelector('.lodge__price').textContent = deal.offer.price + ' $/ночь';
-  offerElement.querySelector('.lodge__type').textContent = translateOfferType(deal.offer.type);
-  offerElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + deal.offer.guests + 'гостей в ' + deal.offer.rooms + ' комнатах';
-  offerElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + deal.offer.checkin + ', выезд до ' + deal.offer.checkout;
-  offerElement.querySelector('.lodge__features').appendChild(features2HTML(deal.offer.features));
-  offerElement.querySelector('.lodge__description').textContent = deal.offer.description;
-  offerElement.querySelector('.lodge__photos').textContent = deal.offer.photos;
+    offerElement.querySelector('.lodge__title').textContent = deal.offer.title;
+    offerElement.querySelector('.lodge__address').textContent = deal.offer.address;
+    offerElement.querySelector('.lodge__price').textContent = deal.offer.price + ' $/ночь';
+    offerElement.querySelector('.lodge__type').textContent = translateOfferType(deal.offer.type);
+    offerElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + deal.offer.guests + 'гостей в ' + deal.offer.rooms + ' комнатах';
+    offerElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + deal.offer.checkin + ', выезд до ' + deal.offer.checkout;
+    offerElement.querySelector('.lodge__features').appendChild(features2HTML(deal.offer.features));
+    offerElement.querySelector('.lodge__description').textContent = deal.offer.description;
+    offerElement.querySelector('.lodge__photos').textContent = deal.offer.photos;
 
-  return offerElement;
-}
+    return offerElement;
+  }
+
+  return {
+    renderOffer : renderOffer
+  }
+})(window, document);
--- js/map.js
+++ js/map.js
@@ -1,24 +1,7 @@
 'use strict';
 
-/**
- * Number.prototype.format(n, x, s, c)
- * 
- * @param {*} integer n: длина десятичной части
- * @param {*} integer x: длина целой части
- * @param {*} mixed   s: разделитель секций
- * @param {*} mixed   c: десятичный разделитель
- */
-Number.prototype.format = function(n, x, s, c) {
-    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
-        num = this.toFixed(Math.max(0, ~~n));
-
-    return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
-};
-
 
 var pinListElement = document.querySelector('.tokyo__pin-map');
-
-
 
 // DOM панель подробного описания предложения по жилью
 var dialogPanel = document.querySelector('.dialog__panel');
@@ -26,55 +9,29 @@ var dialogPanel = document.querySelector('.dialog__panel');
 // div с картиной аватара в описании предложения
 var dialogTitle = document.querySelector('.dialog__title');
 
-
-
 // Объявляем Массив предложений по проживанию
 var offersList = [];
 
 // Наполняем наш массив предложениями
 // наполненеие данных описано в data.js
-fillOfferList(offersList);
+window.data.fillOfferList(offersList);
 
 // DOM элемент первого предложения по жилью
-var firstOffer = renderOffer (offersList[0]);
+var firstOffer = window.card.renderOffer (offersList[0]);
 
 dialogTitle.querySelector('img').src = offersList[0].author.avatar;
-
 
 // Заменяем стандарную панель предложения на первое автомитчески сгенерированное
 dialogPanel.parentNode.replaceChild(firstOffer, dialogPanel);
 
 /**
- * Функция генерирует пин на карте (div элемент с вложенным img)
- * @param {*} offer Объект JS, описывающий предложение по аренде 
- */
-function renderPin (offer) {
-  var pinElement = document.createElement('div');
-  pinElement.className = 'pin';
-  pinElement.style.left = (offer.location.x - 56/2) + 'px';
-  pinElement.style.top = (offer.location.y - 75) + 'px';
-
-  var imgElement = document.createElement('img');
-  imgElement.src = offer.author.avatar;
-  imgElement.tabIndex = 0; // для возможности выделять по tab
-  imgElement.className = 'rounded';
-  imgElement.width = '40';
-  imgElement.height = '40';
-
-  pinElement.insertAdjacentHTML('beforeend', imgElement.outerHTML);
-  //pinElement.appendChild(imgElement);
-  //debugger;
-  return pinElement;
-}
-
-/**
- * Функция заполнения блока DOM-элементами используя renderPin(offersList[i])
+ * Функция заполнения блока DOM-элементами используя renderPin(offersList[i]) из pin.js
  * @return {fragment}
  */
 function fillFragment () {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < 8; i++) {
-    fragment.appendChild(renderPin(offersList[i]));
+    fragment.appendChild(window.pins.renderPin(offersList[i]));
   }
 
   return fragment;
@@ -86,7 +43,7 @@ pinListElement.appendChild(fillFragment());
 // --------------------------------------------------------------
 // Делаем обработчики событий
 // --------------------------------------------------------------
-var dialogClose = dialogTitle.querySelector('.dialog__close');
+var dialogClose = document.querySelector('.dialog__close');
 var dialogForm = document.querySelector('.dialog');
 var pinElements = document.getElementsByClassName('pin');
 var clickedElement = null; // объявляем выбранный элемент на странице (пин)
@@ -102,8 +59,8 @@ clickedElement = pinElements[1]; // текущий выбранный элеме
 //console.log('Количество пинов: ' + pinElements.length);
 
 document.addEventListener('keydown', onEscPress); // добавляет EventListener на ESC
- 
-function pinClickHandler (evt) {
+
+function pinClickHandler(evt) {
   //console.log(evt);
   //debugger;
 
@@ -121,10 +78,10 @@ function pinClickHandler (evt) {
   for (var i = 1; i < pinElements.length; i++) {
     if (pinElements[i].classList.contains('pin--active')) {
       //console.log('Active pin number is ' + (i - 1));
-      
+
       // обновляем панель с информацией по объекту
       // не могу использовать ранее объявленный dialogPanel, видимо после использования appendChild
-      dialogForm.replaceChild(renderOffer (offersList[i - 1]), document.querySelector('.dialog__panel'));
+      dialogForm.replaceChild(window.card.renderOffer(offersList[i - 1]), document.querySelector('.dialog__panel'));
       dialogTitle.querySelector('img').src = offersList[i - 1].author.avatar;
     }
   }
@@ -155,7 +112,7 @@ function closeDialogPanel() {
 
 // добавляем EventListener на каждый из пинов (class='pin')
 for (var i = 0; i < pinElements.length; i++) {
-  if (!pinElements[i].classList.contains('pin__main')){
+  if (!pinElements[i].classList.contains('pin__main')) {
     pinElements[i].addEventListener('click', pinClickHandler, false);
     pinElements[i].addEventListener('keydown', function (evt) {
       if (isActivationEvent(evt)) {
@@ -166,15 +123,15 @@ for (var i = 0; i < pinElements.length; i++) {
   }
 }
 
- function isActivationEvent(evt) {
-   return evt.keyCode === ENTER_KEY_CODE;
- }
+function isActivationEvent(evt) {
+  return evt.keyCode === ENTER_KEY_CODE;
+}
 
 // EventListener для закрытия панели описания объекта
-dialogClose.addEventListener('click', function() {
+dialogClose.addEventListener('click', function () {
   //dialogDialog.classList.add('invisible'); //альтернативный вариант
   dialogForm.style.display = 'none';
-  
+
   // убираем выделение с активного пина, но только если такой существует
   if (clickedElement) {
     clickedElement.classList.remove('pin--active');
@@ -183,103 +140,5 @@ dialogClose.addEventListener('click', function() {
 });
 
 
-// -----------------------------------------------------------------
-// Проверка правильности введённых данных
-// -----------------------------------------------------------------
-var checkinSelectElements = document.querySelector('#time');
-var checkoutSelectedElements = document.querySelector('#timeout');
-var priceElement = document.querySelector('#price');
-var typeElement = document.querySelector('#type');
-var roomNumOptioins = document.querySelector('#room_number');
-var capacityOptions = document.querySelector('#capacity');
-var formElement = document.querySelector('.notice__form');
 
-function init() {
-  checkinSelectElements.addEventListener('click', function () {
-    checkoutSelectedElements.selectedIndex = checkinSelectElements.selectedIndex;
-  });
-
-  checkoutSelectedElements.addEventListener('click', function () {
-    checkinSelectElements.selectedIndex = checkoutSelectedElements.selectedIndex;
-  });
-
-  // Добавляем событие на изменение количества комнат в зависимости от количества возможных гостей
-  capacityOptions.addEventListener('change', guestNumOnChangeHandler);
-
-  // Добавляем событие отслеживания типа жилья - меняем возможную стоимость
-  typeElement.addEventListener('change', typeOnChangeHandler);
-  // прогоняем один раз Handler, чтобы задать правильный placeholder
-  typeOnChangeHandler({ srcElement: { value: 'Квартира' } });
-  priceElement.value = '1000';
-
-  // Добавляем событие на изменение количества возможных гостей в зависимости от количества комнатах
-  roomNumOptioins.addEventListener('change', roomNumOnChangeHandler);
-  roomNumOnChangeHandler(); // и проверяем разок на старте, чтобы сразу правильно отображать
-
-  // Добавляем событие на изменение типа жилья в зависимости от цены за ночь
-  priceElement.addEventListener('change', priceOnChangeHandler);
-}
-
-// добавлем все EventListener и инициируем начальные значения для формы
-init();
-
-
-/**
- * Функция изменения типа жилья в зависимости от цены за ночь
- * @param {*} evt Событие 
- */
-function priceOnChangeHandler(evt) {
-  //debugger;
-  var price = evt.srcElement.value;
-  if (price < 1000) {
-    typeElement.selectedIndex = 1;
-  } else if (price < 10000) {
-    typeElement.selectedIndex = 0;
-  } else {
-    typeElement.selectedIndex = 2;
-  };
-}
-
-
-
-function typeOnChangeHandler(evt) {
-  //debugger;
-  var accomodationType = evt.srcElement.value;
-  if (accomodationType === 'Лачуга') {
-    priceElement.placeholder = '1 - 999'
-    if (priceElement.value >= 1000) {
-      priceElement.value = 999;
-    };
-  } else if (accomodationType === 'Квартира') {
-    priceElement.placeholder = '1000 - 9999'
-    if (priceElement.value < 1000 && priceElement.value != '') {
-      priceElement.value = 1000;
-    } else if (priceElement.value > 10000) {
-      priceElement.value = 9999;
-    };
-  } else { // if accomodationType === 'Дворец'
-    priceElement.placeholder = 'от 10000'
-    if (priceElement.value < 10000 && priceElement.value != '') {
-      priceElement.value = 10000;
-    };
-  };
-}
-
-function roomNumOnChangeHandler() {
-  var guestsOK = (roomNumOptioins.selectedIndex === 0) ? false : true;
-  if (guestsOK) {
-    capacityOptions.selectedIndex = 0; // для 3 гостей
-  } else {
-    capacityOptions.selectedIndex = 1; // не для гостей
-  };
-}
-
-function guestNumOnChangeHandler() {
-  var only1room = (capacityOptions.selectedIndex === 1) ? true : false;
-  if (only1room) {
-    roomNumOptioins.selectedIndex = 0; // не для гостей
-  } else {
-    roomNumOptioins.selectedIndex = 1; // установить в 2 комнаты
-  };
-}
 
