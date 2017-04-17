da68d6a4de39dc1d618cc9c414568de3e40df66a
Pavel Melnik <SESA117590@schneider-electric.com>
2017-04-17 15:23:09 +0300

    Реализован запрос данных с сервера. Исправлены недочёты.

---
+++ js/load.js
@@ -0,0 +1,87 @@
+// load.js
+// load - отрпавка на сервер запросов с помощью XMLHttpRequest
+'use strict'
+// url - стока в которую записан адрес, по которому будет происходить запрос
+// onLoad - функция обратного вызова, которая срабатывает при успешном выполнении запроса
+window.load = (function (window, document) {
+
+  function load(url, onLoad) {
+
+    //var UNSENT = 0; // начальное состояние
+    var OPENED = 1; // вызван open
+    //var HEADER = 2; // получены заголовки
+    //var LOADING = 3; // загружается тело (получен очередной пакет данных)
+    var DONE = 4; // запрос завершён
+
+    var statusBlock = document.querySelector('.status__block');
+    var statusMessage = statusBlock.querySelector('.status__message');
+
+    // создаём новый объект XMLHttpRequest
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+      //Если обмен данными завершен
+      if (request.readyState == DONE) {
+        if (request.status == 200) {
+          statusBlock.style.display = 'none';
+          // передаём управление в onLoad
+          onLoad(request.responseText);
+        }
+        else { // обрабатываем ошибки
+          switch (request.status) {
+            case 0:
+              changeStatusMessage('error', request.status + ': Нет ответа от сервера!'); // no responce
+              break;
+            case 400:
+              changeStatusMessage('error', request.status + ': Плохой запрос!'); //bad request
+              break;
+            case 403:
+              changeStatusMessage('error', request.status + ': Доступ запрещён!');; //forbidden
+              break;
+            case 404:
+              changeStatusMessage('error', request.status + ': Страница не найдена!');; // not found
+              break;
+            case 500:
+              changeStatusMessage('error', request.status + ': Внутрнення ошибка сервера!');; // Internal server error
+            default:
+              changeStatusMessage('error', request.status + ': Какая-то ошибка!');; // some error
+          }
+        }
+      }
+      else {
+        if (request.readyState == OPENED) {
+          statusBlock.style.display = 'block';
+          changeStatusMessage('loading', 'Загрзука данных...');
+          // вывести DIV с сообщение о загрузке данных
+        }
+      }
+    }
+
+    function changeStatusMessage(type, message) {
+      if (type === 'error') {
+        statusMessage.style.color = 'red';
+        statusMessage.style.fontWeight = 'bold';
+        statusMessage.style.animation = 'none';
+        statusMessage.textContent = message;
+      } else if (type === 'loading') {
+        statusMessage.style.color = 'black';
+        statusMessage.style.fontWeight = 'normal';
+        statusMessage.style.animation = 'blinker 1s linear infinite';
+        statusMessage.textContent = message;
+      } else {
+        statusMessage.style.color = 'red';
+        statusMessage.style.fontWeight = 'bold';
+        statusMessage.style.animation = 'none';
+        statusMessage.textContent = 'Выбран неправильный тип';
+      }
+    }
+
+    // конфигурируем GET-запрос на url
+    request.open('GET', url, true);
+    // отсылаем запрос
+    request.send();
+  }
+  return {
+    load: load
+  }
+})(window, document);
--- js/show-card.js
+++ js/show-card.js
@@ -1,26 +1,22 @@
 // show-card.js
 'use strict'
 
-window.showCard = function (offer) {
-  var dialogPanel = document.querySelector('.dialog__panel');
-  var lodgeTitle = dialogPanel.querySelector('.lodge__title');
-  var lodgeAddress = dialogPanel.querySelector('.lodge__address');
-  var lodgePrice = dialogPanel.querySelector('.lodge__price');
-  var lodgeType = dialogPanel.querySelector('.lodge__type');
-  var lodgeRoomsAndGuests = dialogPanel.querySelector('.lodge__rooms-and-guests');
-  var lodgeCheckinTime = dialogPanel.querySelector('.lodge__checkin-time');
-  var lodgeFeatures = dialogPanel.querySelector('.lodge__features');
-  var lodgeDescription = dialogPanel.querySelector('.lodge__description');
-  var lodgePhotos = dialogPanel.querySelector('.lodge__photos');
+window.show_card = (function(window, document) { 
 
-  lodgeTitle.textContent = offer.title;
-  lodgeAddress.textContent = offer.address;
-  lodgePrice.textContent = offer.price;
-  lodgeType.textContent = offer.type;
-  lodgeRoomsAndGuests.textContent = offer.guestsRooms;
-  lodgeCheckinTime.textContent = offer.checkInTime;
-  lodgeFeatures.innerHTML = '';
-  lodgeFeatures.appendChild(offer.features);
-  lodgeDescription.textContent = offer.description;
-  lodgePhotos.textContent = offer.photos;
+function showCard(offer) {
+  window.lodgeTitle.textContent = offer.title;
+  window.lodgeAddress.textContent = offer.address;
+  window.lodgePrice.textContent = offer.price;
+  window.lodgeType.textContent = offer.type;
+  window.lodgeRoomsAndGuests.textContent = offer.guestsRooms;
+  window.lodgeCheckinTime.textContent = offer.checkInTime;
+  window.lodgeFeatures.innerHTML = '';
+  window.lodgeFeatures.appendChild(offer.features);
+  window.lodgeDescription.textContent = offer.description;
+  window.lodgePhotos.innerHTML = '';
+  window.lodgePhotos.appendChild(offer.photos);
 }
+return {
+  showCard: showCard
+}
+})(window, document);
